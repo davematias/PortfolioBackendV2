a5684babf4f5fd30ba2c9ec78e17c3b9c9c7b778
David Matias <davematiasei@gmail.com>
2019-08-29 18:39:36 +0100

    initial template

---
+++ Backend/profile/profile_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"testing"
+	"context"
+	"gotest.tools/assert"
+)
+
+func TestHandler(t *testing.T) {
+	request := context.Context{
+		
+	}
+	data, err := Handler(request)
+}
