70a46fcd320f51ea25347f857c54c6f3c1e983f6
davematias <davematiasei@gmail.com>
2019-09-19 00:00:43 +0100

    fix build

--- Backend/config/envVars.js
+++ Backend/config/envVars.js
@@ -1,9 +1,5 @@
 require('dotenv').config();
 
-console.log("receiver")
-console.log(process.env.EMAIL_RECEIVER)
-console.log(process.env)
-
 module.exports.getEnvVars = () => ({
   EMAIL_RECEIVER: process.env.EMAIL_RECEIVER,
   EMAIL_SENDER: process.env.EMAIL_SENDER,
